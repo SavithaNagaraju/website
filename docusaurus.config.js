3675ff4cf5bebc588fd344efdd2a0cba93b0305f
Savi <nsavi2@gmail.com>
2022-11-04 23:17:37 +0100

    new changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Savitha Nagarajus Site',
+  tagline: 'Dinosaurs may be cool',
   url: 'https://savithanagaraju.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
